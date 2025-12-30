5886fc5dd2a99b52be97d39b437e8bc53b76e95a
ueppoland <uep.poland@gmail.com>
2025-12-30 18:12:23 +0100

    finish 66

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/userRoutes');
 //1) Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 😀');
