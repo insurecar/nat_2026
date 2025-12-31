0daba8a88475568eb64c4e822ba4df70be0a77bc
ueppoland <uep.poland@gmail.com>
2025-12-31 15:27:21 +0100

    finish 85

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ mongoose
 
 const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    uniq: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
