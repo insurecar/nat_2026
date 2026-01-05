135a57ce7a53ba25d27d2900ead77dd6014c04f5
ueppoland <uep.poland@gmail.com>
2026-01-05 09:09:24 +0100

    finish 91

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -54,13 +54,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
