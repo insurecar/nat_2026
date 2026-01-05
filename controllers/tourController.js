b09b30e3f4127bbac349d17f7c5d8e51444557cf
ueppoland <uep.poland@gmail.com>
2026-01-05 10:46:50 +0100

    finish 92

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -74,9 +74,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
