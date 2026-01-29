f5ad0b97d498e62c4de0b2a95e276d2b64ae084d
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-01-29 14:31:24 +0100

    finish 99

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,27 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+
+    //3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //3) Field Limiting
     const tours = await query;
     res.status(200).json({
