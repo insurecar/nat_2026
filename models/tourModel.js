6cafe0b6990f3ffc234bd5730ab5568e3740443e
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-05 15:02:44 +0100

    finish 104

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,64 +1,75 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Should have difficulty'],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [0, 'ratingsAverage must be above or equal to 0'],
+      max: [5, 'ratingsAverage must be below or equal to 5'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
+  {
+    toJSON: { virtuals: true }, //to include virtual properties when data is output as JSON or Object
+    toObject: { virtuals: true }, //to include virtual properties when data is output as JSON or Object
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Should have difficulty'],
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [0, 'ratingsAverage must be above or equal to 0'],
-    max: [5, 'ratingsAverage must be below or equal to 5'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  //virtual property which is not stored in the database but calculated on the fly when we get the data
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
