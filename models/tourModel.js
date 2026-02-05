eee9e05fbd1c83ae158bdbaf537f977f8a2bdf47
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-05 15:49:44 +0100

    finish 105

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -70,6 +73,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   //virtual property which is not stored in the database but calculated on the fly when we get the data
   return this.duration / 7;
+});
+//Document middleware: runs before .save() and .create() but not on .insertMany()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function (next) {
+  console.log('Will save document...');
+  next();
+});
+
+//post middleware : runs after the document is saved to the database
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
