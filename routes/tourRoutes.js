53e87a74a1506bae51f980571d4bff711a592acf
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-05 14:52:26 +0100

    finish 102

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 // router.param('id', tourController.checkId);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/top-5-cheap')
