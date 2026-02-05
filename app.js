b2b6dee3cfa0efe251c6edaa2312a3305453af16
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-05 18:29:23 +0100

    finish 115

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const app = express();
 
 console.log('12536152361523', process.env.PORT);
 
@@ -26,11 +28,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use((err, req, res, next) => {
@@ -42,14 +40,6 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
