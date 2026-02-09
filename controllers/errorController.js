8f2dd18a27f856e38967119afba1048049d76a7c
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-09 16:15:18 +0100

    finish 119

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,9 @@
-const e = require('express');
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -31,6 +36,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err, name: err.name, message: err.message };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    // if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    // if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+    sendErrorProd(error, res);
   }
 };
