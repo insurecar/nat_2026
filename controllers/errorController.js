bcab39d8780cfd719d625a001e018a2b60bae2ec
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-09 15:58:38 +0100

    finish 118

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
+const e = require('express');
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR ', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
