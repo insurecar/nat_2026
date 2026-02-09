b541dd01888460ae127ccefa9c576fb861852b57
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-09 16:36:30 +0100

    finish 120

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,18 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = Object.values(err.keyValue).join(', ');
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,11 +48,20 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err, name: err.name, message: err.message };
+    let error = {
+      ...err,
+      name: err.name,
+      message: err.message,
+      code: err.code,
+      keyValue: err.keyValue,
+      errors: err.errors,
+    };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.code === 11000 || error.code === '11000')
+      error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     // if (error.name === 'JsonWebTokenError') error = handleJWTError();
     // if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
