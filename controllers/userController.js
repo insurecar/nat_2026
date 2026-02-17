f005f8b4f3b7187082f889f719cefb00acfbc2d0
Rostyslav.Shyian.External <rostyslav.shyian.external@jetbrains.com>
2026-02-17 17:25:29 +0100

    finish 130

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'fail',
-    message: 'This rout has not implemented yet',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'fail',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-// router.post('/signup', authController.signup);
 router.route('/signup').post(authController.signup);
-//
+router.route('/login').post(authController.login);
+
 router
   .route('/')
   .get(userController.getAllUsers)
